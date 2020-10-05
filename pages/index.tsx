58c5042809a4e10b94d69f22f08976f47ac30f0d
matthewcobbing <matthewc@nandos.co.uk>
2020-10-05 22:27:50 +0100

    add lake to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Box, Card, CardMedia } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+});
 
 const Home: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <Layout>
-        Hello
+      <Box width={1}>
+        <Card className={classes.root}>
+          <CardMedia
+            style={{ height: 5, paddingTop: "56%" }}
+            image="/images/lake.jpg"
+          />
+        </Card>
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
 export default Home;
