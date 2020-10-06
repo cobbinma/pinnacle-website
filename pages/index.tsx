a9094140c1c957cd37e07b604ba6e955e7d1fdd9
matthewcobbing <matthewc@nandos.co.uk>
2020-10-06 08:55:03 +0100

    services home page section

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,9 @@ const useStyles = makeStyles({
   content: {
     marginBottom: 20,
   },
+  section: {
+    marginBottom: 70,
+  },
 });
 
 const Home: React.FC = () => {
@@ -64,7 +67,13 @@ const Home: React.FC = () => {
         justify="flex-start"
         alignItems="stretch"
       >
-        <Grid container direction="row" justify="center" alignItems="center">
+        <Grid
+          className={classes.section}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
           <Grid item xs={10} md={8}>
             <div>
               <Typography className={classes.content} variant="h2">
@@ -88,6 +97,40 @@ const Home: React.FC = () => {
               </Typography>
               <Button variant="contained" color="primary">
                 About us
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid
+          className={classes.section}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={10} md={8}>
+            <div>
+              <Typography className={classes.content} variant="h2">
+                Services
+              </Typography>
+              <Typography
+                className={classes.content}
+                variant="h5"
+                component="p"
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </Typography>
+              <Button variant="contained" color="primary">
+                Services
               </Button>
             </div>
           </Grid>
